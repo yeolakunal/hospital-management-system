22f93e5ad7c65442877800771d41489865a20465
Kunal Yeola <yeolakunal@gmail.com>
2021-11-10 11:55:32 +0530

    Added grid to display patient details

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Datagrid from "./components/Datagrid";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <Datagrid />
+        </div>
+    );
+};
+
+export default App;
